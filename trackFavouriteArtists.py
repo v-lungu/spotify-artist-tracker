4041ae3ee4efd875f0eabeff90a02c838376e91d
Vlad.Lungu <vlad.lungu@fdmgroup.com>
2025-03-15 16:33:51 -0400

    get all new tracks done

--- trackFavouriteArtists.py
+++ trackFavouriteArtists.py
@@ -1,9 +1,7 @@
 import json
 import base64
-from requests import post
-
-msg = "Roll a dice!"
-print(msg)
+from requests import get, post
+from datetime import datetime, timedelta
 
 def get_token(client_id, client_secret):
    auth_string = client_id + ":" + client_secret
@@ -26,29 +24,57 @@ def get_auth_headers(token):
 
 
 
-def get_artist_music_since_last_run(token, date, artist):
-  print("helli")
-  return ["hello"]
+def get_artist_music_since_last_run(headers, date, artist):
+  url = "https://api.spotify.com/v1/search"
+
+  year_end = datetime.now().year
+  if (date):
+     date = datetime.strptime(date, '%Y-%m-%d')
+     year_start =   date.year
+  else :
+     date = datetime.today()
+     year_start = datetime.now().year - 1
+     date = (date - timedelta(days=31))
+
+  queryUrl = url + f"?q=artist%3A{artist}+year%3A{year_start}-{year_end}&type=track&limit=50"
+  result = get(queryUrl, headers=headers)
+  result_json = json.loads(result.content)["tracks"]["items"]
+
+  tracks = dict()
+
+  for result in result_json:
+     date_released = datetime.strptime(result.get('album').get('release_date'), '%Y-%m-%d')
+     name = result.get('name')
+     if date_released > date:
+        tracks[name] = result.get('uri')
+        print(result.get('name'))
+
+
+  return tracks
 
 
 
 
 def main():
-   info_file = open('info.json')
+   info_file = open("info.json", 'r+')
    info_json = json.load(info_file)
 
-   token = get_token(info_json["clientId"], info_json["clientSecret"])
+   token = get_token(info_json['clientId'], info_json['clientSecret'])
    headers = get_auth_headers(token)
 
    artists = info_json["artists"]
    date = info_json["lastRun"]
+   
+   tracks = dict()
    for artist in artists:
-    music = get_artist_music_since_last_run(token, date, artist)
+    tracks.update(get_artist_music_since_last_run(headers, date, artist))
 
 
 
 
-
+   # info_json["lastRun"] = datetime.today().strftime('%Y-%m-%d')
+   # info_file.seek(0)
+   # json.dump(info_json, info_file, ensure_ascii=False, indent=4)
    info_file.close()
 
 if __name__ == "__main__":
@@ -56,8 +82,10 @@ if __name__ == "__main__":
 
 # goal: get songs since x by all artists I specify
 # steps
-# 1. get list of artists 
+# 1. get list of artists  x 
 # 2. for each artist: 
-#     a. search for tracks in last 1-2 years
+#     a. search for tracks in last 1-2 years x
 #         eg. q=artist:Sammy Virji year:2025 // https://api.spotify.com/v1/search?q=artist%3ASammy+Virji+year%3A2025&type=track
 #     b. filter tracks by release date since x 
+# 3. create playlist
+# 4. add all songs to playlist
